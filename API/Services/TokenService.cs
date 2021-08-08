1b0815486f26eb3da93549b9725d1411d83fe01c
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-08-08 14:58:07 -0400

    Create UserDto and return token to user

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -27,7 +27,8 @@ namespace API.Services
 
             var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
-            var tokenDescriptor = new SecurityTokenDescriptor{
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
                 Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.Now.AddDays(7),
                 SigningCredentials = creds
