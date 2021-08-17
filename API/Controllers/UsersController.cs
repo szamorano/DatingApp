4ec8b54ea2656ba7aa9fde3114a95eeabf0c36bb
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-08-17 17:47:25 -0400

    Use CreatedAtRoute method for 201 status response

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -36,7 +36,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -80,9 +80,11 @@ namespace API.Controllers
             user.Photos.Add(photo);
 
             if (await _userRepository.SaveAllSync())
-                return _mapper.Map<PhotoDto>(photo);
+            {
+                return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
 
-            return BadRequest("Problem adding phoro");
+            return BadRequest("Problem adding photo");
         }
     }
 }
