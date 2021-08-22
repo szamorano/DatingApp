7b0f414e329d575bd4aaaf5aea8c04ea1685011b
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-08-21 21:06:51 -0400

    Add sorting by created and lastActive

--- API/Data/UserRespository.cs
+++ API/Data/UserRespository.cs
@@ -42,8 +42,14 @@ namespace API.Data
 
             query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
-                return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .AsNoTracking(), userParams.PageNumber, userParams.PageSize);
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                 _ => query.OrderByDescending(u => u.LastActive)
+            };
+
+            return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .AsNoTracking(), userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
