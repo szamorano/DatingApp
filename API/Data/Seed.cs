40ef8443008281f85b8887cd89ea4fa660a1633f
Steven Zamorano <stevenzamorano.code@gmail.com>
2021-08-25 19:26:13 -0400

    Setting up the entities to use .Net Identity

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -18,11 +18,7 @@ namespace API.Data
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             foreach (var user in users)
             {
-                using var hmac = new HMACSHA512();
-
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Passw0rd"));
-                user.PasswordSalt = hmac.Key;
 
                 context.Users.Add(user);
             }
